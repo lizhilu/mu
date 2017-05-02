c3d5e9a7326e196400010f1f08e8c07b492a6158
Lizhi Lu <lizhilu@usc.edu>
2017-05-02 15:28:15 -0700

    decoder5 update

--- src/decoder/decode5.java
+++ src/decoder/decode5.java
@@ -1,4 +1,4 @@
-package decoder;
+//package decode;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -19,121 +19,19 @@ public class decode5 {
 	static JLabel lbIm1;
 	static JLabel lbIm2;
 	static BufferedImage img;
+	static BufferedImage original;
 	final static   double pi=Math.PI;
-	 static  int q1=50;
-	 static int q2=10000;
+	 static  int q1=1000;
+	 static int q2=10;
 	 
 	 static double[][] blockXU=new double[8][8];
 	 static double[][] blockYV=new double[8][8];
-//	public static void main(String[] args){
-		
-		//getVideo();
-		
-//		String filename= "output.cmp";		
-////		String filename= "output.cmp";		
-//        String line="";
-//        
-//        int frameRate=15;
-//      
-//        //预先计算idct公式里重复的部分
-//        for(int x=0;x<8;x++){
-//        	for(int y=0;y<8;y++){
-//        		 for(int u=0;u<8;u++){
-//        	        	for(int v=0;v<8;v++){
-//        	        		blockXU[x][u]=Math.cos((2*x+1)*u*pi/16.0);
-//        	        		blockYV[y][v]=Math.cos((2*y+1)*v*pi/16.0);
-//        	        	}
-//        	        }
-//        	}
-//        }
-//        
-//        List<BufferedImage> outputVideo=new ArrayList<BufferedImage>();
-//        img = new BufferedImage(960, 540, BufferedImage.TYPE_INT_RGB);	
-//        //一帧8160行
-//	      try
-//	       {
-//              BufferedReader in=new BufferedReader(new FileReader(filename));
-//               line=in.readLine();
-//                int blockCount=0;
-//            	long startTime=System.currentTimeMillis();   //获取开始时间  
-//                while (line!=null)
-//               {               	              	               
-//                      idct(line,blockCount);
-//                	  blockCount++;
-//                	  if(blockCount==8160){
-//                		  long endTime=System.currentTimeMillis(); //获取结束时间  
-//                          System.out.println("程序运行时间： "+(endTime-startTime)+"ms");   
-//                		  outputVideo.add(img);
-//                		  blockCount=0;
-//                		  img = new BufferedImage(960, 540, BufferedImage.TYPE_INT_RGB);
-//                			 startTime=System.currentTimeMillis();   //获取开始时间  
-//                	  }else{
-//                		  
-//                	  }
-//                	  line=in.readLine();       		    
-//                     
-//               }
-//              
-//               
-//                in.close();
-//	        } catch (IOException e)
-//	         {
-//	               e.printStackTrace();
-//	        }
-//	      
-//	      
-//	 
-//		    JFrame frame = new JFrame();
-//		    GridBagLayout gLayout = new GridBagLayout();
-//		    frame.getContentPane().setLayout(gLayout);
-//		    
-//		    JLabel lbText1 = new JLabel("Video height: ");
-//		    lbText1.setHorizontalAlignment(SwingConstants.CENTER);
-//			
-//		    lbIm1 = new JLabel(new ImageIcon(img));
-//			GridBagConstraints c = new GridBagConstraints();
-//			c.fill = GridBagConstraints.HORIZONTAL;
-//			c.anchor = GridBagConstraints.CENTER;
-//			c.weightx = 0.5;
-//			c.gridx = 0;
-//			c.gridy = 0;
-//			frame.getContentPane().add(lbText1, c);		
-//			c.fill = GridBagConstraints.HORIZONTAL;
-//			c.gridx = 0;
-//			c.gridy = 1;
-//			frame.getContentPane().add(lbIm1, c);
-//			
-//			frame.pack();
-//			frame.setVisible(true);
-//		
-//			for(int i=1; i < outputVideo.size(); i++) {
-//		    	lbIm1.setIcon(new ImageIcon(outputVideo.get(i)));
-//		    	try {
-//		    		Thread.sleep(1000/frameRate);
-//		    	} catch(InterruptedException e) {
-//		    		e.printStackTrace();
-//		    	}
-//		    }
-//			while(true){
-//			 for(int i=0; i < outputVideo.size(); i++) {
-//			    	lbIm1.setIcon(new ImageIcon(outputVideo.get(i)));
-//			    	try {
-//			    		Thread.sleep(1000/frameRate);
-//			    	} catch(InterruptedException e) {
-//			    		e.printStackTrace();
-//			    	}
-//			    }
-//			}
-		    
-		    
-//	}
-	
-	public static List<BufferedImage> getVideo(String filename) {
-		
+	public static void main(String[] args){
+		String filename= "output1.cmp";		
 //		String filename= "output.cmp";		
         String line="";
         
-        int frameRate=15;
+        int frameRate = 30;
       
         //预先计算idct公式里重复的部分
         for(int x=0;x<8;x++){
@@ -148,7 +46,10 @@ public class decode5 {
         }
         
         List<BufferedImage> outputVideo=new ArrayList<BufferedImage>();
-        img = new BufferedImage(960, 540, BufferedImage.TYPE_INT_RGB);	
+        List<BufferedImage> originalVideo=new ArrayList<BufferedImage>();
+        
+        img = new BufferedImage(960, 540, BufferedImage.TYPE_INT_RGB);
+        original = new BufferedImage(960, 540, BufferedImage.TYPE_INT_RGB);	
         //一帧8160行
 	      try
 	       {
@@ -160,12 +61,14 @@ public class decode5 {
                {               	              	               
                       idct(line,blockCount);
                 	  blockCount++;
-                	  if(blockCount==8160){
-                		  long endTime=System.currentTimeMillis(); //获取结束时间  
+                	  if(blockCount == 8160){
+                		  long endTime = System.currentTimeMillis(); //获取结束时间  
                           System.out.println("程序运行时间： "+(endTime-startTime)+"ms");   
                 		  outputVideo.add(img);
+                		  originalVideo.add(original);
                 		  blockCount=0;
                 		  img = new BufferedImage(960, 540, BufferedImage.TYPE_INT_RGB);
+                		  original = new BufferedImage(960, 540, BufferedImage.TYPE_INT_RGB);
                 			 startTime=System.currentTimeMillis();   //获取开始时间  
                 	  }else{
                 		  
@@ -181,51 +84,50 @@ public class decode5 {
 	               e.printStackTrace();
 	        }
 	      
-	      return outputVideo;
 	      
-//	 
-//		    JFrame frame = new JFrame();
-//		    GridBagLayout gLayout = new GridBagLayout();
-//		    frame.getContentPane().setLayout(gLayout);
-//		    
-//		    JLabel lbText1 = new JLabel("Video height: ");
-//		    lbText1.setHorizontalAlignment(SwingConstants.CENTER);
-//			
-//		    lbIm1 = new JLabel(new ImageIcon(img));
-//			GridBagConstraints c = new GridBagConstraints();
-//			c.fill = GridBagConstraints.HORIZONTAL;
-//			c.anchor = GridBagConstraints.CENTER;
-//			c.weightx = 0.5;
-//			c.gridx = 0;
-//			c.gridy = 0;
-//			frame.getContentPane().add(lbText1, c);		
-//			c.fill = GridBagConstraints.HORIZONTAL;
-//			c.gridx = 0;
-//			c.gridy = 1;
-//			frame.getContentPane().add(lbIm1, c);
-//			
-//			frame.pack();
-//			frame.setVisible(true);
-//		
-//			for(int i=1; i < outputVideo.size(); i++) {
-//		    	lbIm1.setIcon(new ImageIcon(outputVideo.get(i)));
-//		    	try {
-//		    		Thread.sleep(1000/frameRate);
-//		    	} catch(InterruptedException e) {
-//		    		e.printStackTrace();
-//		    	}
-//		    }
-//			while(true){
-//			 for(int i=0; i < outputVideo.size(); i++) {
-//			    	lbIm1.setIcon(new ImageIcon(outputVideo.get(i)));
-//			    	try {
-//			    		Thread.sleep(1000/frameRate);
-//			    	} catch(InterruptedException e) {
-//			    		e.printStackTrace();
-//			    	}
-//			    }
-//			}
-	      
+	 
+		    JFrame frame = new JFrame();
+		    GridBagLayout gLayout = new GridBagLayout();
+		    frame.getContentPane().setLayout(gLayout);
+		    
+		    JLabel lbText1 = new JLabel("Video height: ");
+		    lbText1.setHorizontalAlignment(SwingConstants.CENTER);
+			
+		    lbIm1 = new JLabel(new ImageIcon(img));
+			GridBagConstraints c = new GridBagConstraints();
+			c.fill = GridBagConstraints.HORIZONTAL;
+			c.anchor = GridBagConstraints.CENTER;
+			c.weightx = 0.5;
+			c.gridx = 0;
+			c.gridy = 0;
+			frame.getContentPane().add(lbText1, c);		
+			c.fill = GridBagConstraints.HORIZONTAL;
+			c.gridx = 0;
+			c.gridy = 1;
+			frame.getContentPane().add(lbIm1, c);
+			
+			frame.pack();
+			frame.setVisible(true);
+		
+			for(int i=1; i < outputVideo.size(); i++) {
+		    	lbIm1.setIcon(new ImageIcon(outputVideo.get(i)));
+		    	try {
+		    		Thread.sleep(1000/frameRate);
+		    	} catch(InterruptedException e) {
+		    		e.printStackTrace();
+		    	}
+		    }
+			while(true){
+			 for(int i=0; i < outputVideo.size(); i++) {
+			    	lbIm1.setIcon(new ImageIcon(outputVideo.get(i)));
+			    	try {
+			    		Thread.sleep(1000/frameRate);
+			    	} catch(InterruptedException e) {
+			    		e.printStackTrace();
+			    	}
+			    }
+			}
+		    
 		    
 	}
 	
@@ -243,13 +145,18 @@ public class decode5 {
         String[] coe=line.split(" ");
         int blockType=Integer.valueOf(coe[0]);
         int[] qcoe=new int[193];
+        
+        int[] qc = new int[193];
+        
         if(blockType==0){
         	 for(int i=0;i<coe.length;i++){
              	qcoe[i]=(int)((Double.valueOf(coe[i])/q1))*q1;
+             	qc[i]=(int)((Double.valueOf(coe[i])/1))*1;
              }
         }else{
         	 for(int i=0;i<coe.length;i++){
               	qcoe[i]=(int)((Double.valueOf(coe[i])/q2))*q2;
+              	qc[i]=(int)((Double.valueOf(coe[i])/1))*1;
               }
         }      
 //        if(blockType==1){
@@ -259,14 +166,18 @@ public class decode5 {
      
         for(int i=1;i<65;i++){
        	 double fxyr=0;double fxyg=0;double fxyb=0;
+       	 double fr=0;double fg=0;double fb=0;
        	 int x=(i-1)/8;int y=(i-1)%8;
 //       	 System.out.println("x  y "+x+" "+y);
-       	 for(int u=0;u<8;u++){
+       	 for(int u=0;u<8;u++){ 
        		 double ci = ((u==0)?1.0/Math.sqrt(2.0):1.0);	
        		 for(int v=0;v<8;v++){       			
                 	double cj = ((v==0)?1.0/Math.sqrt(2.0):1.0);
                 	double cc=ci*cj;
                 	double cos=blockXU[x][u]*blockXU[y][v];
+                	fr+=cc* qc[u*8+v+1]*cos;
+					fg+=cc* qc[u*8+v+65]*cos;
+               		fb+=cc* qc[u*8+v+129]*cos;
                 	if(blockType==0){
                 		 fxyr+=cc* qcoe[u*8+v+1]*cos;
                			 fxyg+=cc* qcoe[u*8+v+65]*cos;
@@ -283,8 +194,56 @@ public class decode5 {
        	 int r=(int) (0.25*fxyr);
        	 int g=(int) (0.25*fxyg);
        	 int b=(int) (0.25*fxyb);
+       	 int rr=(int) (0.25*fr);
+       	 int gg=(int) (0.25*fg);
+       	 int bb=(int) (0.25*fb);
+       	 //System.out.println("r is " + r);
+       	 //System.out.println("g is " + g);
+       	 //System.out.println("b is " + b);
 //       	 System.out.println(r+" "+g+" "+b);
+		
+		if (r < 0) {
+			r = r + 127;
+		}
+		if (g < 0) {
+			g = g + 127;
+		}
+		if (b < 0) {
+			b = b + 127;
+		}
+		if (r > 255) {
+			r = r - 127;
+		}
+		if (g > 255) {
+			g = g - 127;
+		}
+		if (b > 255) {
+			b = b - 127;
+		}
+		if (rr < 0) {
+			rr = rr + 127;
+		}
+		if (gg < 0) {
+			gg = gg + 127;
+		}
+		if (bb < 0) {
+			bb = bb + 127;
+		}
+		if (rr > 255) {
+			rr = rr - 127;
+		}
+		if (gg > 255) {
+			gg = gg - 127;
+		}
+		if (bb > 255) {
+			bb = bb - 127;
+		}
+		//if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
+			//System.out.println("overflow: (" + r + ", " + g + ", "+ b + ")");
+		//}
        	 int pix = 0xff000000 | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
+       	 int pi = 0xff000000 | ((rr & 0xff) << 16) | ((gg & 0xff) << 8) | (bb & 0xff);
+       	 //System.out.println("pixel is " + pix);
 //       	 img.setRGB(x, y, pix);
 //    	 img.setRGB(x+8, y, pix);
 //    	 img.setRGB(x, y+8, pix);
@@ -295,24 +254,29 @@ public class decode5 {
        	 	case 0:
        	 		if((actualx+x<960)&&(actualy+y)<540){
        	 			img.setRGB(actualx+x, actualy+y, pix);
+       	 			original.setRGB(actualx+x, actualy+y, pi);
        	 		}
        	 		break;
        	 	case 1:
        	 		if((actualx+x+8<960)&&(actualy+y)<540){
        	 			img.setRGB(actualx+x+8, actualy+y, pix);
+       	 			original.setRGB(actualx+x+8, actualy+y, pi);
        	 		}
        	 		break;
        	 	case 2: 
 	       	 	if((actualx+x<960)&&(actualy+y+8)<540){
 	       	 		img.setRGB(actualx+x, actualy+y+8, pix);
+	       	 		original.setRGB(actualx+x, actualy+y+8, pi);
 	   	 		}
 	       	 	break;    	 	
        	 	case 3: 
 	       	 	if((actualx+x+8<960)&&(actualy+y+8)<540){
-	       	 	img.setRGB(actualx+x+8, actualy+y+8, pix);
+	       	 		img.setRGB(actualx+x+8, actualy+y+8, pix);
+		   	 		original.setRGB(actualx+x+8, actualy+y+8, pi);
 	   	 		}
        	 		break;
        	 }  	
+       
     	
         }
 	}
